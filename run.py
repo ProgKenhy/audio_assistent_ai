192b334feb32066d63e6978a7dd7dd13584aa36c
Alexander Chereshenko <sashok20053@gmail.com>
2026-05-19 21:06:15 +0200

    add logging

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+import logging
+
 from game.app import App
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s %(name)s: %(message)s",
+)
 
 if __name__ == "__main__":
     App().run()
